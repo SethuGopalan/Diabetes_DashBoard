05f93260e806ac139d8797eb21a3a35215dc6e73
Sethu Gopalan <41025798+SethuGopalan@users.noreply.github.com>
2026-05-27 03:10:02 +0000

    update api code

---
+++ src/api/main.py
@@ -0,0 +1,23 @@
+# Import FastAPI framework for creating APIs
+from fastapi import FastAPI 
+
+# Import uvicorn server for running the FastAPI application
+import uvicorn
+
+# Create FastAPI application object
+app = FastAPI()
+
+# Create GET endpoint for root URL "/"
+# When user visits localhost:8000/
+# this function will run
+@app.get("/")
+
+# Function that handles requests for "/"
+def root_endpoint():
+
+    # Return JSON response back to browser/client
+    return {"status": "API running"}git 
+
+
+  
+    
